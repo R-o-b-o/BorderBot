2775a0d3be9667bb5e30a0c1ae06f41844fa5bb8
R-o-b-o <39059788+R-o-b-o@users.noreply.github.com>
2019-04-07 22:21:16 +0100

    fix

--- fileHandler.py
+++ fileHandler.py
@@ -6,14 +6,15 @@ async def downloadAvatar(author):
     filepath = "avatars/" + str(author.id) 
     if not os.path.exists(filepath):
         os.makedirs(filepath)
+    url = str(author.avatar_url)
 
-    if author.avatar_url.endswith(".gif?size=1024"):
+    if url.endswith(".gif?size=1024"):
         filepath += "/" + author.avatar + ".gif"
     else:
         filepath += "/" + author.avatar + ".webp"
     
     async with aiohttp.ClientSession() as session:
-        async with session.get(author.avatar_url) as r:
+        async with session.get(url) as r:
             if r.status == 200:
                 f = await aiofiles.open(filepath, mode='wb')
                 await f.write(await r.read())
